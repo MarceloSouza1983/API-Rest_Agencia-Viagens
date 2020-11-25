df4419f36d7c76bbcd8f1e890e61d85dba2c10c0
monthalcantara <montival_junior@yahoo.com.br>
2020-11-25 20:07:12 -0300

    criacao do projeto

---
+++ src/test/java/io/github/monthalcantara/agenciaviagem/AgenciaviagemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.agenciaviagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaviagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
